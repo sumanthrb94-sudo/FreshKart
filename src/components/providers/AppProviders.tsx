7130311279f04d25157e2aebcf20896ba9024fce
sumanthrb94-sudo <sumanthrb94@gmail.com>
2026-07-06 19:55:10 +0530

    feat: integrate NotificationProvider for in-app notifications

--- src/components/providers/AppProviders.tsx
+++ src/components/providers/AppProviders.tsx
@@ -7,6 +7,7 @@ import { AuthProvider } from "./AuthProvider";
 import { CartProvider } from "./CartProvider";
 import { ThemeProvider } from "./ThemeProvider";
 import { OrderTrackerProvider } from "./OrderTrackerProvider";
+import { NotificationProvider } from "./NotificationProvider";
 import { AiChatAgent } from "@/components/AiChatAgent";
 import { ToastContainer } from "@/components/ToastContainer";
 import { OrderTracker } from "@/components/OrderTracker";
@@ -22,10 +23,12 @@ export function AppProviders({ children }: { children: React.ReactNode }) {
         <AuthProvider>
           <CartProvider>
             <OrderTrackerProvider>
-              {children}
-              <AiChatAgent />
-              <ToastContainer />
-              <OrderTracker />
+              <NotificationProvider>
+                {children}
+                <AiChatAgent />
+                <ToastContainer />
+                <OrderTracker />
+              </NotificationProvider>
             </OrderTrackerProvider>
           </CartProvider>
         </AuthProvider>
